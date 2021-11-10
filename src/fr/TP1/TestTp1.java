9416b45d6e292a9dc1376574021c0898c1267a39
Joktaa <joris.rouziere@gmail.com>
2021-11-10 11:29:50 +0100

    bugfix

--- src/fr/TP1/TestTp1.java
+++ src/fr/TP1/TestTp1.java
@@ -33,8 +33,9 @@ public class TestTp1 {
         assertEquals(-9, Main.MonMin(tableau));
     }
 
+     @Test
     public void testSort() {
-        ArrayList<Integer> tableauTrie = new ArrayList<>(Arrays.asList(-9, 1, 2, 4, 5, 9, 12));
+        ArrayList<Integer> tableauTrie = new ArrayList<>(Arrays.asList(-9, 1, 2, 4, 5, 8, 12));
         assertEquals(tableauTrie, Main.trie(tableau));
     }
 }
