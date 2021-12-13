4d3751a6ba8d19d5274f0dbc8d14c5d60b10ca29
Joktaa <joris.rouziere@gmail.com>
2021-12-13 11:38:21 +0100

    exo 1 fichiers

---
+++ src/fr/Fichiers/Main.java
@@ -0,0 +1,10 @@
+package fr.Fichiers;
+
+
+public class Main {
+
+    public static char statistiqueMot() {
+        // Renvoyer la lettre la plus utilisée dans le dictionnaire "liste_francais.txt"
+        return ' ';
+    }
+}
