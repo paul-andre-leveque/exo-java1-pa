65a24fa7dfa8eeff70e525ee310b43b756c4b515
joris <joris.rouziere@gmail.com>
2022-01-05 10:28:38 +0100

    ajout conseils

--- src/fr/Fichiers/Main.java
+++ src/fr/Fichiers/Main.java
@@ -5,6 +5,9 @@ public class Main {
 
     public static char statistiqueMot() {
         // Renvoyer la lettre la plus utilisée dans le dictionnaire "liste_francais.txt"
+        // Étape 1 : Lire le fichier, et afficher les mots dans le terminal
+        // Étape 2 : Choisir un type de liste pouvant stocker les caractères et leur nombre associé
+        // Étape 3 : Définir quel caractère est le plus utilisé, et le renvoyer
         return ' ';
     }
 }
