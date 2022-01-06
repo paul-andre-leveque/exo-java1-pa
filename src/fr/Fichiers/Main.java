33c164036f43c7f530b2b5fe606a8a8f0f59f426
joris.rouziere <joris.rouziere@scalian.com>
2022-01-06 11:02:41 +0100

    add info Fichiers

--- src/fr/Fichiers/Main.java
+++ src/fr/Fichiers/Main.java
@@ -6,7 +6,7 @@ public class Main {
     public static char statistiqueMot() {
         // Renvoyer la lettre la plus utilisée dans le dictionnaire "liste_francais.txt"
         // Étape 1 : Lire le fichier, et afficher les mots dans le terminal
-        // Étape 2 : Choisir un type de liste pouvant stocker les caractères et leur nombre associé
+        // Étape 2 : Choisir un type de liste pouvant stocker les caractères et leur nombre associé, puis remplir cette liste
         // Étape 3 : Définir quel caractère est le plus utilisé, et le renvoyer
         return ' ';
     }
